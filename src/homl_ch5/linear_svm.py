af44d9d815ef61a0b137be2b1219989e1f992506
duffymo <duffymo@gmail.com>
2023-04-07 14:42:02 -0400

    SVM for making moons showing contour using SO solution.

--- src/homl_ch5/linear_svm.py
+++ src/homl_ch5/linear_svm.py
@@ -2,10 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+
+
 from sklearn import datasets
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.svm import LinearSVC
+
 
 def classify_iris():
     iris = datasets.load_iris()
@@ -35,8 +38,27 @@ def classify_moons():
     grouped = df.groupby('label')
     for key, group in grouped:
         group.plot(ax=ax, kind='scatter', x='x', y='y', label=key, color=colors[key])
+
+    polynomial_svm_clf = Pipeline((
+        ('poly_features', PolynomialFeatures(degree=3)),
+        ('scalar', StandardScaler()),
+        ('svm_clf', LinearSVC(C=10, loss='hinge'))
+    ))
+    polynomial_svm_clf.fit(X, y)
+
+    # see https://stackoverflow.com/questions/43778380/how-to-draw-decision-boundary-in-svm-sklearn-data-in-python
+    npoints = 1000
+    xmin, xmax = X[:, 0].min() - 1, X[:, 0].max() + 1
+    ymin, ymax = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(
+        np.arange(xmin, xmax, (xmax - xmin) / npoints),
+        np.arange(ymin, ymax, (ymax - ymin) / npoints)
+    )
+    Z = polynomial_svm_clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
     plt.show()
-    return X, y
+
 
 if __name__ == '__main__':
-    X, y = classify_moons()
+    classify_moons()
