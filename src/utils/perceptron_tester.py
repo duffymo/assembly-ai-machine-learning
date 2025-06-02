cb0cf3582e293a1bd3e3bdc7c442233cf09f3e15
duffymo <duffymo@gmail.com>
2025-06-01 20:41:01 -0400

    Added link spelling out what specificity means.

--- src/utils/perceptron_tester.py
+++ src/utils/perceptron_tester.py
@@ -1,8 +1,22 @@
 def perceptron(x1, x2, w1, w2, b):
     return int((x1*w1 + x2*w2 + b) > 0)
 
+
+"""
+Function to test NOR perceptron implementations
+"""
+def nor_perceptron(w1, w2, b):
+    return [perceptron(0.0, 0.0, w1, w2, b),
+            perceptron(0.0, 1.0, w1, w2, b),
+            perceptron(1.0, 0.0, w1, w2, b),
+            perceptron(1.0, 1.0, w1, w2, b)]
 """
 Function to test perceptron weights from bnomial site 
 """
 if __name__ == "__main__":
-    print(perceptron(1, 1, 1, 1, 1))
+    w1 = [-1.0, 0.5, -2.0]
+    w2 = [-1.0, 0.5, -1.0]
+    b = [1.0, -0.1, 0.8]
+    for j in range(0, 3):
+        print(j, nor_perceptron(w1[j], w2[j], b[j]))
+
